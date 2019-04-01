939700c7edc8af576edd3c68b614fe684c671c7d
cb-aravindkumar <aravindkumar@chargebee.com>
2019-04-01 15:08:21 +0530

    data type icons

--- target/classes/static/javascript/App.js
+++ target/classes/static/javascript/App.js
@@ -5,7 +5,11 @@ var App = new function () {
     var _this = this;
     this.DIV = document.createElement("div");
 
-    this.ICON = ["save", "edit", "delete", "add", "table", "close", "refresh", "expand", "contract", "more", "done", "setting", "dashboard", "barchart", "piechart"];
+    this.ICON = [
+    	"save", "edit", "delete", "add", "table", "close", "refresh", "expand", "contract", "more", "done", "setting", "dashboard", 
+    	"barchart", "piechart", 
+    	"numberdatatype", "decimaldatatype", "percentdatatype", "textdatatype", "multilinetextdatatype", "emaildatatype", "currencydatatype", "datedatatype", "urldatatype"
+    	];
     this.ICON_SUFFIX = ["", "--fill", "--round", "--sharp"];
 //    this.ICON_SUFFIX = ["", "--fill", "--sharp"];
 
